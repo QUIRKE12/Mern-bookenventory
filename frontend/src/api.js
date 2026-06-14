288e50a9311d1a0ad39fbc190245a0d4da4297ce
QUIRKE12 <salvatorahishakiye1@gmail.com>
2026-06-14 18:55:58 +0200

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,47 +1,46 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const BASE_URL = import.meta.env.VITE_API_URL;
 
-// Get all books
-export const getAllBooks = async () => {
-  const res = await fetch(`${BASE_URL}/all-books`);
-  if (!res.ok) throw new Error("Failed to fetch books");
+// Get all products
+export const getAllProducts = async () => {
+  const res = await fetch(`${BASE_URL}/all-products`);
+  if (!res.ok) throw new Error("Failed to fetch products");
   return res.json();
 };
 
-
-// Get single book
-export const getBookById = async (id) => {
-  const res = await fetch(`${BASE_URL}/books/${id}`);
-  if (!res.ok) throw new Error("Failed to fetch book");
+// Get single product
+export const getProductById = async (id) => {
+  const res = await fetch(`${BASE_URL}/products/${id}`);
+  if (!res.ok) throw new Error("Failed to fetch product");
   return res.json();
 };
 
-// Upload new book
-export const createBook = async (bookData) => {
-  const res = await fetch(`${BASE_URL}/upload-book`, {
+// Upload new product
+export const createProduct = async (productData) => {
+  const res = await fetch(`${BASE_URL}/upload-product`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(bookData),
+    body: JSON.stringify(productData),
   });
-  if (!res.ok) throw new Error("Failed to create book");
+  if (!res.ok) throw new Error("Failed to create product");
   return res.json();
 };
 
-// Update book
-export const updateBook = async (id, updatedData) => {
-  const res = await fetch(`${BASE_URL}/book/${id}`, {
+// Update product
+export const updateProduct = async (id, updatedData) => {
+  const res = await fetch(`${BASE_URL}/product/${id}`, {
     method: "PATCH",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(updatedData),
   });
-  if (!res.ok) throw new Error("Failed to update book");
+  if (!res.ok) throw new Error("Failed to update product");
   return res.json();
 };
 
-// Delete book
-export const deleteBook = async (id) => {
-  const res = await fetch(`${BASE_URL}/book/${id}`, {
+// Delete product
+export const deleteProduct = async (id) => {
+  const res = await fetch(`${BASE_URL}/product/${id}`, {
     method: "DELETE",
   });
-  if (!res.ok) throw new Error("Failed to delete book");
+  if (!res.ok) throw new Error("Failed to delete product");
   return res.json();
 };
