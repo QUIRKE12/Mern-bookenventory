38122827db44e03c9fe09f6536276c84000ce8e5
QUIRKE12 <salvatorahishakiye1@gmail.com>
2026-06-14 18:56:45 +0200

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { RouterProvider } from 'react-router-dom';
-import router from './routers/router.jsx';
-import AuthProvider from './contexts/AuthProvider.jsx';  // Correct default import
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routers/router";
+import AuthProvider from "./contexts/AuthProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>  {/* Wrap your RouterProvider with AuthProvider */}
+    <AuthProvider>
       <RouterProvider router={router} />
     </AuthProvider>
   </React.StrictMode>
