c5bb34cedde120d5d11c8f81c04c44c2eca6b765
QUIRKE12 <salvatorahishakiye1@gmail.com>
2026-06-15 14:20:10 +0200

    Update BestSellerProducts.jsx

--- frontend/src/home/BestSellerProducts.jsx
+++ frontend/src/home/BestSellerProducts.jsx
@@ -1,1 +1,20 @@
+import { useEffect, useState } from "react";
+import ProductCards from "../components/ProductCards";
 
+const BestSellerProducts = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}/all-products`)
+      .then((res) => res.json())
+      .then((data) => setProducts(data.slice(0, 8)));
+  }, []);
+
+  return (
+    <div className="px-4 lg:px-24 my-16">
+      <ProductCards products={products} headline="Best Seller Products" />
+    </div>
+  );
+};
+
+export default BestSellerProducts;
