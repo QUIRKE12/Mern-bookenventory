53738b6fac5145cd546fad499b1c15d58eeb65a4
QUIRKE12 <salvatorahishakiye1@gmail.com>
2026-06-15 12:23:08 +0200

    Update BarnnerCard.jsx

--- frontend/src/home/BarnnerCard.jsx
+++ frontend/src/home/BarnnerCard.jsx
@@ -1,32 +1,26 @@
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/effect-cards';
 import './BarnnerCard.css';
-
-
-// import required modules
 import { EffectCards } from 'swiper/modules';
 
 const BarnnerCard = () => {
   return (
     <div className='banner'>
-<Swiper
+      <Swiper
         effect={'cards'}
         grabCursor={true}
         modules={[EffectCards]}
         className="mySwiper"
       >
- <SwiperSlide className="swiper-slide-1"></SwiperSlide>
-<SwiperSlide className="swiper-slide-2"></SwiperSlide>
-<SwiperSlide className="swiper-slide-3"></SwiperSlide>
-<SwiperSlide className="swiper-slide-4"></SwiperSlide>
-<SwiperSlide className="swiper-slide-5"></SwiperSlide>
-
+        <SwiperSlide />
+        <SwiperSlide />
+        <SwiperSlide />
+        <SwiperSlide />
+        <SwiperSlide />
       </Swiper>
-          </div>
-  )
-}
+    </div>
+  );
+};
 
-export default BarnnerCard
+export default BarnnerCard;
