af27fba5fa0f11115f375fd8f1be52d0c86fbd3b
QUIRKE12 <salvatorahishakiye1@gmail.com>
2026-06-15 14:08:07 +0200

    Update MyFooter.jsx

--- frontend/src/components/MyFooter.jsx
+++ frontend/src/components/MyFooter.jsx
@@ -1,60 +1,67 @@
 import { Footer } from "flowbite-react";
-import {BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs"
+import {
+  BsFacebook,
+  BsInstagram,
+  BsTwitter,
+  BsWhatsapp,
+} from "react-icons/bs";
 
 const MyFooter = () => {
   return (
     <Footer bgDark>
       <div className="w-full px-4 lg:px-24">
-        <div className="grid w-full grid-cols-2 gap-8 px-6 py-8 md:grid-cols-4">
+        <div className="grid w-full grid-cols-1 gap-8 px-6 py-8 md:grid-cols-4">
           <div>
-            <Footer.Title title="Company" />
+            <Footer.Title title="GIGO COMPANY LTD" />
             <Footer.LinkGroup col>
-              <Footer.Link href="#">About</Footer.Link>
-              <Footer.Link href="#">Careers</Footer.Link>
-              <Footer.Link href="#">Brand Center</Footer.Link>
-              <Footer.Link href="#">Blog</Footer.Link>
+              <Footer.Link href="/about">Turi Bande?</Footer.Link>
+              <Footer.Link href="/shop">Ibinyobwa</Footer.Link>
+              <Footer.Link href="/blog">Amakuru</Footer.Link>
+              <Footer.Link href="/contact">Twandikire</Footer.Link>
             </Footer.LinkGroup>
           </div>
           <div>
-            <Footer.Title title="help center" />
+            <Footer.Title title="Ibinyobwa" />
             <Footer.LinkGroup col>
-              <Footer.Link href="#">Discord Server</Footer.Link>
-              <Footer.Link href="#">Twitter</Footer.Link>
-              <Footer.Link href="#">Facebook</Footer.Link>
-              <Footer.Link href="#">Contact Us</Footer.Link>
+              <Footer.Link href="#">Ibinyobwa Vyambiye</Footer.Link>
+              <Footer.Link href="#">Ibinyobwa Bitambiye</Footer.Link>
+              <Footer.Link href="#">Amananiza</Footer.Link>
+              <Footer.Link href="#">Ibishasha</Footer.Link>
             </Footer.LinkGroup>
           </div>
           <div>
-            <Footer.Title title="legal" />
+            <Footer.Title title="Twandikire" />
+            <Footer.LinkGroup col>
+              <Footer.Link href="#">+257 XX XX XX XX</Footer.Link>
+              <Footer.Link href="#">info@gigocompany.com</Footer.Link>
+              <Footer.Link href="#">Bujumbura, Burundi</Footer.Link>
+            </Footer.LinkGroup>
+          </div>
+          <div>
+            <Footer.Title title="Amategeko" />
             <Footer.LinkGroup col>
               <Footer.Link href="#">Privacy Policy</Footer.Link>
-              <Footer.Link href="#">Licensing</Footer.Link>
-              <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
-            </Footer.LinkGroup>
-          </div>
-          <div>
-            <Footer.Title title="download" />
-            <Footer.LinkGroup col>
-              <Footer.Link href="#">iOS</Footer.Link>
-              <Footer.Link href="#">Android</Footer.Link>
-              <Footer.Link href="#">Windows</Footer.Link>
-              <Footer.Link href="#">MacOS</Footer.Link>
+              <Footer.Link href="#">Terms & Conditions</Footer.Link>
+              <Footer.Link href="#">Customer Support</Footer.Link>
             </Footer.LinkGroup>
           </div>
         </div>
-        <div className="w-full bg-gray-700 px-4 py-6 sm:flex sm:items-center sm:justify-between">
-          <Footer.Copyright href="#" by="Flowbite™" year={2022} />
-          <div className="mt-4 flex space-x-6 sm:mt-0 sm:justify-center">
+        <div className="w-full border-t border-gray-600 px-4 py-6 sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="GIGO COMPANY LIMITED"
+            year={new Date().getFullYear()}
+          />
+          <div className="mt-4 flex space-x-6 sm:mt-0">
             <Footer.Icon href="#" icon={BsFacebook} />
             <Footer.Icon href="#" icon={BsInstagram} />
             <Footer.Icon href="#" icon={BsTwitter} />
-            <Footer.Icon href="#" icon={BsGithub} />
+            <Footer.Icon href="#" icon={BsWhatsapp} />
           </div>
         </div>
       </div>
     </Footer>
   );
-}
+};
 
-
-export default MyFooter
+export default MyFooter;
