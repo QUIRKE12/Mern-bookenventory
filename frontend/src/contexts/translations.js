ac16c7a193bec8d36ff70ee1ece2eb9fb55f9330
QUIRKE12 <salvatorahishakiye1@gmail.com>
2026-06-21 14:50:07 +0200

    Create translations.js

---
+++ frontend/src/contexts/translations.js
@@ -0,0 +1,48 @@
+// Navbar translations: Kirundi/Kinyarwanda (rn), English (en), French (fr)
+export const translations = {
+  rn: {
+    home: "Ahabanza",
+    products: "Ibicuruzwa",
+    about: "Ibitwerekeye",
+    news: "Amakuru",
+    orders: "Commande",
+    myOrders: "Commande Zanje",
+    myAccount: "Konti Yanje",
+    login: "Injira",
+    signup: "Iyandikishe",
+    logout: "Sohoka",
+    user: "Umukoresha",
+  },
+  en: {
+    home: "Home",
+    products: "Products",
+    about: "About",
+    news: "News",
+    orders: "Orders",
+    myOrders: "My Orders",
+    myAccount: "My Account",
+    login: "Sign In",
+    signup: "Sign Up",
+    logout: "Sign Out",
+    user: "User",
+  },
+  fr: {
+    home: "Accueil",
+    products: "Produits",
+    about: "À propos",
+    news: "Actualités",
+    orders: "Commandes",
+    myOrders: "Mes Commandes",
+    myAccount: "Mon Compte",
+    login: "Connexion",
+    signup: "S'inscrire",
+    logout: "Déconnexion",
+    user: "Utilisateur",
+  },
+};
+
+export const languageOptions = [
+  { code: "rn", label: "Kirundi" },
+  { code: "en", label: "English" },
+  { code: "fr", label: "Français" },
+];
