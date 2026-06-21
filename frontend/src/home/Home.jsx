a218fec2db5a60752ed8a7e12544ca088306698d
QUIRKE12 <salvatorahishakiye1@gmail.com>
2026-06-21 11:07:03 +0200

    Update Home.jsx

--- frontend/src/home/Home.jsx
+++ frontend/src/home/Home.jsx
@@ -1,13 +1,11 @@
 import Banner from "./Banner";
 import BestSellerProducts from "./BestSellerProducts";
-import FavProducts from "./FavProducts";
 
 const Home = () => {
   return (
-    <div style={{ background: "#fff", minHeight: "100vh" }}>
+    <div style={{ background: "#FAFAFA", minHeight: "100vh" }}>
       <Banner />
       <BestSellerProducts />
-      <FavProducts />
     </div>
   );
 };
