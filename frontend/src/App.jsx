eda6192f80bd22776d9b0a706c67c27d884dc281
QUIRKE12 <salvatorahishakiye1@gmail.com>
2026-06-21 12:26:54 +0200

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import MyFooter from "./components/MyFooter";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <div className="gigo-layout min-h-screen">
         <Outlet />
